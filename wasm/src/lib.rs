71ffaf4eb2a30b29a482251d8b4efa7d23c0cb86
IsaacDynamo <isaacdynamo@gmail.com>
2024-07-14 19:55:28 +0200

    Made all functions inspectable

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,13 +3,14 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn uiua_to_dot(msg: String) -> String {
+pub fn uiua_to_dot(msg: String) -> JsValue {
     let flows = uiua_flow::process(&msg);
 
-    // For now return the first function
-    for flow in flows {
-        return uiua_flow::plot(&flow.1);
+    let obj = js_sys::Object::new();
+    for (k,v) in flows.iter() {
+        let key = JsValue::from(k);
+        let value = JsValue::from(uiua_flow::plot(&v));
+        js_sys::Reflect::set(&obj, &key, &value).unwrap();
     }
-
-    "".to_string()
+    JsValue::from(obj)
 }
